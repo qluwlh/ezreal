97b10869566cbc67907e5ead3907919116269599
wanglihua <wanglihua@innobuddy.com>
2021-08-24 23:55:45 +0800

    [Update]: typescript template

--- packages/cra-template-typescript/template/src/App.tsx
+++ packages/cra-template-typescript/template/src/App.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import classes from "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={classes.App}>
+      <header className={classes["App-header"]}>
+        <img src={logo} className={classes["App-logo"]} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={classes["App-link"]}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
