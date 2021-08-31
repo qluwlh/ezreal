146a7017b52180ce6cf95a36218dd27ff64dad6e
wanglihua <wanglihua@innobuddy.com>
2021-08-31 18:44:56 +0800

    [Update]: eslint-config-react-prettier-ts

--- packages/eslint-config-react-prettier-ts/index.js
+++ packages/eslint-config-react-prettier-ts/index.js
@@ -1,21 +1,15 @@
-console.log(`🚀eslint-plugin🚀`);
-
 module.exports = {
-  parser: "@typescript-eslint/parser",
   extends: [
     "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
+    "airbnb",
     "plugin:prettier/recommended",
   ],
-  plugins: ["@typescript-eslint"],
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-    jest: true,
-    node: true,
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
   },
   settings: {
     react: {
@@ -23,26 +17,13 @@ module.exports = {
       version: "detect",
     },
   },
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true,
   },
-  rules: {
-    // "@typescript-eslint/explicit-module-boundary-types": "off",
-    // "@typescript-eslint/no-var-requires": 0,
-    // "react/react-in-jsx-scope": 0,
-    // "@typescript-eslint/explicit-function-return-type": 0,
-    // "@typescript-eslint/no-empty-function": 0,
-    // "@typescript-eslint/no-explicit-any": 0,
-    // "@typescript-eslint/no-unused-vars": 0,
-    // "@typescript-eslint/no-use-before-define": 0,
-    // "@typescript-eslint/class-name-casing": 0,
-    // "@typescript-eslint/triple-slash-reference": 0,
-    // "react-hooks/rules-of-hooks": "warn",
-    // "react-hooks/exhaustive-deps": "off",
-    "prettier/prettier": ["error"],
-  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {},
 };
