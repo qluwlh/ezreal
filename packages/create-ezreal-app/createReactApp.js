d0ef0b9efc6e588ddbf54afe4e07fa5cdd9de145
wanglihua <wanglihua@innobuddy.com>
2021-09-06 16:21:53 +0800

    chore: 删除淘宝镜像

--- packages/create-ezreal-app/createReactApp.js
+++ packages/create-ezreal-app/createReactApp.js
@@ -1,103 +1,88 @@
-"use strict";
+'use strict'
 
-const chalk = require("chalk");
-const commander = require("commander");
-const fs = require("fs-extra");
-const os = require("os");
-const path = require("path");
-const spawn = require("cross-spawn");
-const validateProjectName = require("validate-npm-package-name");
-const packageJson = require("./package.json");
+const chalk = require('chalk')
+const commander = require('commander')
+const fs = require('fs-extra')
+const os = require('os')
+const path = require('path')
+const spawn = require('cross-spawn')
+const validateProjectName = require('validate-npm-package-name')
+const packageJson = require('./package.json')
 
-let projectName;
+let projectName
 
 function init() {
   const program = new commander.Command(packageJson.name)
     .version(packageJson.version)
-    .arguments("<project-directory>")
-    .usage(`${chalk.green("<project-directory>")} [options]`)
+    .arguments('<project-directory>')
+    .usage(`${chalk.green('<project-directory>')} [options]`)
     .action((name) => {
-      projectName = name;
+      projectName = name
     })
     .allowUnknownOption()
-    .on("--help", () => {
-      console.log(`${chalk.green("<project-directory>")} is required.`);
+    .on('--help', () => {
+      console.log(`${chalk.green('<project-directory>')} is required.`)
     })
-    .parse(process.argv);
+    .parse(process.argv)
 
-  if (typeof projectName === "undefined") {
-    console.error("Please specify the project directory:");
-    process.exit(1);
+  if (typeof projectName === 'undefined') {
+    console.error('Please specify the project directory:')
+    process.exit(1)
   }
-  createApp(projectName);
+  createApp(projectName)
 }
 
 function createApp(name) {
-  const root = path.resolve(name);
-  const appName = path.basename(root);
-  checkAppName(appName);
-  fs.ensureDirSync(name);
+  const root = path.resolve(name)
+  const appName = path.basename(root)
+  checkAppName(appName)
+  fs.ensureDirSync(name)
   if (!isSafeToCreateProjectIn(root, name)) {
-    process.exit(1);
+    process.exit(1)
   }
 
-  console.log();
-  console.log(`Creating a new React app in ${chalk.green(root)}.`);
-  console.log();
+  console.log()
+  console.log(`Creating a new React app in ${chalk.green(root)}.`)
+  console.log()
 
   const packageJson = {
     name: appName,
-    version: "0.1.0",
+    version: '0.1.0',
     private: true,
-  };
-  fs.writeFileSync(
-    path.join(root, "package.json"),
-    JSON.stringify(packageJson, null, 2) + os.EOL
-  );
-  const originalDirectory = process.cwd();
-  process.chdir(root);
-  run(root, appName, originalDirectory);
+  }
+  fs.writeFileSync(path.join(root, 'package.json'), JSON.stringify(packageJson, null, 2) + os.EOL)
+  const originalDirectory = process.cwd()
+  process.chdir(root)
+  run(root, appName, originalDirectory)
 }
 
 function install(root, dependencies) {
   return new Promise((resolve, reject) => {
-    const command = "yarnpkg";
-    const args = [
-      "--registry",
-      "https://registry.npm.taobao.org",
-      "add",
-      ...dependencies,
-      "--cwd",
-      root,
-    ];
-    const child = spawn(command, args, { stdio: "inherit" });
-    child.on("close", (code) => {
+    const command = 'yarnpkg'
+    const args = ['add', ...dependencies, '--cwd', root]
+    const child = spawn(command, args, { stdio: 'inherit' })
+    child.on('close', (code) => {
       if (code !== 0) {
-        reject({ command: `${command} ${args.join(" ")}` });
-        return;
+        reject({ command: `${command} ${args.join(' ')}` })
+        return
       }
-      resolve();
-    });
-  });
+      resolve()
+    })
+  })
 }
 
 const run = async (root, appName, originalDirectory) => {
   const [packageToInstall, templateToInstall] = await Promise.all([
     getInstallPackage(),
     getTemplateInstallPackage(),
-  ]);
-  const allDependencies = [
-    "react",
-    "react-dom",
-    packageToInstall,
-    templateToInstall,
-  ];
+  ])
+  const allDependencies = ['react', 'react-dom', packageToInstall, templateToInstall]
 
-  console.log("Installing packages. This might take a couple of minutes.");
+  console.log('Installing packages. This might take a couple of minutes.')
 
-  await install(root, allDependencies);
+  await install(root, allDependencies)
 
-  const nodeArgs = [];
+  const nodeArgs = []
   await executeNodeScript(
     {
       cwd: process.cwd(),
@@ -108,38 +93,37 @@ const run = async (root, appName, originalDirectory) => {
     var init = require('${packageToInstall}/scripts/init.js');
     init.apply(null, JSON.parse(process.argv[1]));
   `
-  );
-};
+  )
+}
 function executeNodeScript({ cwd, args }, data, source) {
   return new Promise((resolve, reject) => {
-    const child = spawn(
-      process.execPath,
-      [...args, "-e", source, "--", JSON.stringify(data)],
-      { cwd, stdio: "inherit" }
-    );
-    child.on("close", (code) => {
+    const child = spawn(process.execPath, [...args, '-e', source, '--', JSON.stringify(data)], {
+      cwd,
+      stdio: 'inherit',
+    })
+    child.on('close', (code) => {
       if (code !== 0) {
-        reject({ command: `node ${args.join(" ")}` });
-        return;
+        reject({ command: `node ${args.join(' ')}` })
+        return
       } else {
-        resolve();
+        resolve()
       }
-    });
-  });
+    })
+  })
 }
 
 function getInstallPackage() {
-  let packageToInstall = "@wanglihua/react-scripts";
-  return Promise.resolve(packageToInstall);
+  let packageToInstall = '@wanglihua/react-scripts'
+  return Promise.resolve(packageToInstall)
 }
 
-function getTemplateInstallPackage(template = "typescript") {
-  let templateToInstall = `@wanglihua/cra-template-${template}`;
-  return Promise.resolve(templateToInstall);
+function getTemplateInstallPackage(template = 'typescript') {
+  let templateToInstall = `@wanglihua/cra-template-${template}`
+  return Promise.resolve(templateToInstall)
 }
 
 function checkAppName(appName) {
-  const validationResult = validateProjectName(appName);
+  const validationResult = validateProjectName(appName)
   if (!validationResult.validForNewPackages) {
     console.error(
       chalk.red(
@@ -147,19 +131,16 @@ function checkAppName(appName) {
           `"${appName}"`
         )} because of npm naming restrictions:\n`
       )
-    );
-    [
-      ...(validationResult.errors || []),
-      ...(validationResult.warnings || []),
-    ].forEach((error) => {
-      console.error(chalk.red(`  * ${error}`));
-    });
-    console.error(chalk.red("\nPlease choose a different project name."));
-    process.exit(1);
+    )
+    ;[...(validationResult.errors || []), ...(validationResult.warnings || [])].forEach((error) => {
+      console.error(chalk.red(`  * ${error}`))
+    })
+    console.error(chalk.red('\nPlease choose a different project name.'))
+    process.exit(1)
   }
 
   // TODO: there should be a single place that holds the dependencies
-  const dependencies = ["react", "react-dom", "react-scripts"].sort();
+  const dependencies = ['react', 'react-dom', 'react-scripts'].sort()
   if (dependencies.includes(appName)) {
     console.error(
       chalk.red(
@@ -168,42 +149,38 @@ function checkAppName(appName) {
         )} because a dependency with the same name exists.\n` +
           `Due to the way npm works, the following names are not allowed:\n\n`
       ) +
-        chalk.cyan(dependencies.map((depName) => `  ${depName}`).join("\n")) +
-        chalk.red("\n\nPlease choose a different project name.")
-    );
-    process.exit(1);
+        chalk.cyan(dependencies.map((depName) => `  ${depName}`).join('\n')) +
+        chalk.red('\n\nPlease choose a different project name.')
+    )
+    process.exit(1)
   }
 }
 
 function isSafeToCreateProjectIn(root, name) {
   const validFiles = [
-    ".DS_Store",
-    ".git",
-    ".gitattributes",
-    ".gitignore",
-    ".gitlab-ci.yml",
-    ".hg",
-    ".hgcheck",
-    ".hgignore",
-    ".idea",
-    ".npmignore",
-    ".travis.yml",
-    "docs",
-    "LICENSE",
-    "README.md",
-    "mkdocs.yml",
-    "Thumbs.db",
-  ];
+    '.DS_Store',
+    '.git',
+    '.gitattributes',
+    '.gitignore',
+    '.gitlab-ci.yml',
+    '.hg',
+    '.hgcheck',
+    '.hgignore',
+    '.idea',
+    '.npmignore',
+    '.travis.yml',
+    'docs',
+    'LICENSE',
+    'README.md',
+    'mkdocs.yml',
+    'Thumbs.db',
+  ]
   // These files should be allowed to remain on a failed install, but then
   // silently removed during the next create.
-  const errorLogFilePatterns = [
-    "npm-debug.log",
-    "yarn-error.log",
-    "yarn-debug.log",
-  ];
+  const errorLogFilePatterns = ['npm-debug.log', 'yarn-error.log', 'yarn-debug.log']
   const isErrorLog = (file) => {
-    return errorLogFilePatterns.some((pattern) => file.startsWith(pattern));
-  };
+    return errorLogFilePatterns.some((pattern) => file.startsWith(pattern))
+  }
 
   const conflicts = fs
     .readdirSync(root)
@@ -211,43 +188,39 @@ function isSafeToCreateProjectIn(root, name) {
     // IntelliJ IDEA creates module files before CRA is launched
     .filter((file) => !/\.iml$/.test(file))
     // Don't treat log files from previous installation as conflicts
-    .filter((file) => !isErrorLog(file));
+    .filter((file) => !isErrorLog(file))
 
   if (conflicts.length > 0) {
-    console.log(
-      `The directory ${chalk.green(name)} contains files that could conflict:`
-    );
-    console.log();
+    console.log(`The directory ${chalk.green(name)} contains files that could conflict:`)
+    console.log()
     for (const file of conflicts) {
       try {
-        const stats = fs.lstatSync(path.join(root, file));
+        const stats = fs.lstatSync(path.join(root, file))
         if (stats.isDirectory()) {
-          console.log(`  ${chalk.blue(`${file}/`)}`);
+          console.log(`  ${chalk.blue(`${file}/`)}`)
         } else {
-          console.log(`  ${file}`);
+          console.log(`  ${file}`)
         }
       } catch (e) {
-        console.log(`  ${file}`);
+        console.log(`  ${file}`)
       }
     }
-    console.log();
-    console.log(
-      "Either try using a new directory name, or remove the files listed above."
-    );
+    console.log()
+    console.log('Either try using a new directory name, or remove the files listed above.')
 
-    return false;
+    return false
   }
 
   // Remove any log files from a previous installation.
   fs.readdirSync(root).forEach((file) => {
     if (isErrorLog(file)) {
-      fs.removeSync(path.join(root, file));
+      fs.removeSync(path.join(root, file))
     }
-  });
-  return true;
+  })
+  return true
 }
 
 module.exports = {
   init,
   getTemplateInstallPackage,
-};
+}
--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -1,341 +1,310 @@
-"use strict";
+'use strict'
 
-process.on("unhandledRejection", (err) => {
-  throw err;
-});
+process.on('unhandledRejection', (err) => {
+  throw err
+})
 
-const fs = require("fs-extra");
-const path = require("path");
-const chalk = require("react-dev-utils/chalk");
-const execSync = require("child_process").execSync;
-const spawn = require("react-dev-utils/crossSpawn");
-const { defaultBrowsers } = require("react-dev-utils/browsersHelper");
-const os = require("os");
+const fs = require('fs-extra')
+const path = require('path')
+const chalk = require('react-dev-utils/chalk')
+const execSync = require('child_process').execSync
+const spawn = require('react-dev-utils/crossSpawn')
+const { defaultBrowsers } = require('react-dev-utils/browsersHelper')
+const os = require('os')
 
-const verifyTypeScriptSetup = require("./utils/verifyTypeScriptSetup");
+const verifyTypeScriptSetup = require('./utils/verifyTypeScriptSetup')
 function isInGitRepository() {
   try {
-    execSync("git rev-parse --is-inside-work-tree", { stdio: "ignore" });
-    return true;
+    execSync('git rev-parse --is-inside-work-tree', { stdio: 'ignore' })
+    return true
   } catch (e) {
-    return false;
+    return false
   }
 }
 
 function isInMercurialRepository() {
   try {
-    execSync("hg --cwd . root", { stdio: "ignore" });
-    return true;
+    execSync('hg --cwd . root', { stdio: 'ignore' })
+    return true
   } catch (e) {
-    return false;
+    return false
   }
 }
 
 function tryGitInit() {
   try {
-    execSync("git --version", { stdio: "ignore" });
+    execSync('git --version', { stdio: 'ignore' })
     if (isInGitRepository() || isInMercurialRepository()) {
-      return false;
+      return false
     }
 
-    execSync("git init", { stdio: "ignore" });
-    return true;
+    execSync('git init', { stdio: 'ignore' })
+    return true
   } catch (e) {
-    console.warn("Git repo not initialized", e);
-    return false;
+    console.warn('Git repo not initialized', e)
+    return false
   }
 }
 
 function tryGitCommit(appPath) {
   try {
-    execSync("git add -A", { stdio: "ignore" });
-    execSync(
-      'git commit -m "build(init): 使用@wanglihua/create-ezreal-app初始化项目"',
-      { stdio: "ignore" }
-    );
-    return true;
+    execSync('git add -A', { stdio: 'ignore' })
+    execSync('git commit -m "build(init): 使用@wanglihua/create-ezreal-app初始化项目"', {
+      stdio: 'ignore',
+    })
+    return true
   } catch (e) {
-    console.warn("Git commit not created", e);
-    console.warn("Removing .git directory...");
+    console.warn('Git commit not created', e)
+    console.warn('Removing .git directory...')
     try {
       // unlinkSync() doesn't work on directories.
-      fs.removeSync(path.join(appPath, ".git"));
+      fs.removeSync(path.join(appPath, '.git'))
     } catch (removeErr) {
       // Ignore.
     }
-    return false;
+    return false
   }
 }
 
-module.exports = function (
-  appPath,
-  appName,
-  verbose,
-  originalDirectory,
-  templateName
-) {
-  const appPackage = require(path.join(appPath, "package.json"));
-  const useYarn = fs.existsSync(path.join(appPath, "yarn.lock"));
+module.exports = function (appPath, appName, verbose, originalDirectory, templateName) {
+  const appPackage = require(path.join(appPath, 'package.json'))
+  const useYarn = fs.existsSync(path.join(appPath, 'yarn.lock'))
 
   if (!templateName) {
-    console.log("");
+    console.log('')
     console.error(
       `A template was not provided. This is likely because you're using an outdated version of ${chalk.cyan(
-        "create-react-app"
+        'create-react-app'
       )}.`
-    );
-    return;
+    )
+    return
   }
 
   const templatePath = path.dirname(
     require.resolve(`${templateName}/package.json`, { paths: [appPath] })
-  );
-  const templateJsonPath = path.join(templatePath, "template.json");
+  )
+  const templateJsonPath = path.join(templatePath, 'template.json')
 
-  let templateJson = {};
+  let templateJson = {}
   if (fs.existsSync(templateJsonPath)) {
-    templateJson = require(templateJsonPath);
+    templateJson = require(templateJsonPath)
   }
 
-  const templatePackage = templateJson.package || {};
+  const templatePackage = templateJson.package || {}
 
   // Keys to ignore in templatePackage
   const templatePackageBlacklist = [
-    "name",
-    "version",
-    "description",
-    "keywords",
-    "bugs",
-    "license",
-    "author",
-    "contributors",
-    "files",
-    "browser",
-    "bin",
-    "man",
-    "directories",
-    "repository",
-    "peerDependencies",
-    "bundledDependencies",
-    "optionalDependencies",
-    "engineStrict",
-    "os",
-    "cpu",
-    "preferGlobal",
-    "private",
-    "publishConfig",
-  ];
+    'name',
+    'version',
+    'description',
+    'keywords',
+    'bugs',
+    'license',
+    'author',
+    'contributors',
+    'files',
+    'browser',
+    'bin',
+    'man',
+    'directories',
+    'repository',
+    'peerDependencies',
+    'bundledDependencies',
+    'optionalDependencies',
+    'engineStrict',
+    'os',
+    'cpu',
+    'preferGlobal',
+    'private',
+    'publishConfig',
+  ]
 
   // Keys from templatePackage that will be merged with appPackage
-  const templatePackageToMerge = ["devDependencies", "dependencies", "scripts"];
+  const templatePackageToMerge = ['devDependencies', 'dependencies', 'scripts']
 
-  const templatePackageToReplace = Object.keys(templatePackage).filter(
-    (key) => {
-      return (
-        !templatePackageBlacklist.includes(key) &&
-        !templatePackageToMerge.includes(key)
-      );
-    }
-  );
+  const templatePackageToReplace = Object.keys(templatePackage).filter((key) => {
+    return !templatePackageBlacklist.includes(key) && !templatePackageToMerge.includes(key)
+  })
 
-  const templateScripts = templatePackage.scripts || {};
+  const templateScripts = templatePackage.scripts || {}
   appPackage.scripts = {
-    start: "ezreal start",
-    build: "ezreal build",
+    start: 'ezreal start',
+    build: 'ezreal build',
     ...templateScripts,
-  };
+  }
 
-  appPackage.dependencies = appPackage.dependencies || {};
+  appPackage.dependencies = appPackage.dependencies || {}
 
   if (useYarn) {
     appPackage.scripts = Object.entries(appPackage.scripts).reduce(
       (acc, [key, value]) => ({
         ...acc,
-        [key]: value.replace(/(npm run |npm )/, "yarn "),
+        [key]: value.replace(/(npm run |npm )/, 'yarn '),
       }),
       {}
-    );
+    )
   }
 
-  appPackage.browserslist = defaultBrowsers;
+  appPackage.browserslist = defaultBrowsers
 
   templatePackageToReplace.forEach((key) => {
-    appPackage[key] = templatePackage[key];
-  });
+    appPackage[key] = templatePackage[key]
+  })
 
-  fs.writeFileSync(
-    path.join(appPath, "package.json"),
-    JSON.stringify(appPackage, null, 2) + os.EOL
-  );
+  fs.writeFileSync(path.join(appPath, 'package.json'), JSON.stringify(appPackage, null, 2) + os.EOL)
 
   // Copy the files for the user
-  const templateDir = path.join(templatePath, "template");
+  const templateDir = path.join(templatePath, 'template')
   if (fs.existsSync(templateDir)) {
-    fs.copySync(templateDir, appPath);
+    fs.copySync(templateDir, appPath)
   } else {
-    console.error(
-      `Could not locate supplied template: ${chalk.green(templateDir)}`
-    );
-    return;
+    console.error(`Could not locate supplied template: ${chalk.green(templateDir)}`)
+    return
   }
   // modifies README.md commands based on user used package manager.
   if (useYarn) {
     try {
-      const readme = fs.readFileSync(path.join(appPath, "README.md"), "utf8");
+      const readme = fs.readFileSync(path.join(appPath, 'README.md'), 'utf8')
       fs.writeFileSync(
-        path.join(appPath, "README.md"),
-        readme.replace(/(npm run |npm )/g, "yarn "),
-        "utf8"
-      );
+        path.join(appPath, 'README.md'),
+        readme.replace(/(npm run |npm )/g, 'yarn '),
+        'utf8'
+      )
     } catch (err) {
       // Silencing the error. As it fall backs to using default npm commands.
     }
   }
 
-  const gitignoreExists = fs.existsSync(path.join(appPath, ".gitignore"));
+  const gitignoreExists = fs.existsSync(path.join(appPath, '.gitignore'))
   if (gitignoreExists) {
-    const data = fs.readFileSync(path.join(appPath, "gitignore"));
-    fs.appendFileSync(path.join(appPath, ".gitignore"), data);
-    fs.unlinkSync(path.join(appPath, "gitignore"));
+    const data = fs.readFileSync(path.join(appPath, 'gitignore'))
+    fs.appendFileSync(path.join(appPath, '.gitignore'), data)
+    fs.unlinkSync(path.join(appPath, 'gitignore'))
   } else {
     // Rename gitignore after the fact to prevent npm from renaming it to .npmignore
     // See: https://github.com/npm/npm/issues/1862
-    fs.moveSync(
-      path.join(appPath, "gitignore"),
-      path.join(appPath, ".gitignore"),
-      []
-    );
+    fs.moveSync(path.join(appPath, 'gitignore'), path.join(appPath, '.gitignore'), [])
   }
 
-  let initializedGit = false;
+  let initializedGit = false
 
   if (tryGitInit()) {
-    initializedGit = true;
-    console.log();
-    console.log("Initialized a git repository.");
+    initializedGit = true
+    console.log()
+    console.log('Initialized a git repository.')
   }
 
-  let command;
-  let remove;
-  let args;
-  let depArgs = [];
-  let devDepArgs = [];
+  let command
+  let remove
+  let args
+  let depArgs = []
+  let devDepArgs = []
   if (useYarn) {
-    command = "yarnpkg";
-    remove = "remove";
-    args = ["--registry", "https://registry.npm.taobao.org", "add"];
+    command = 'yarnpkg'
+    remove = 'remove'
+    args = ['add']
   } else {
-    command = "npm";
-    remove = "uninstall";
+    command = 'npm'
+    remove = 'uninstall'
     args = [
-      "install",
-      "--no-audit", // https://github.com/facebook/create-react-app/issues/11174
-      "--save",
-      verbose && "--verbose",
-    ].filter(Boolean);
+      'install',
+      '--no-audit', // https://github.com/facebook/create-react-app/issues/11174
+      '--save',
+      verbose && '--verbose',
+    ].filter(Boolean)
   }
 
-  const dependenciesToInstall = Object.entries(
-    templatePackage.dependencies || {}
-  );
+  const dependenciesToInstall = Object.entries(templatePackage.dependencies || {})
   if (dependenciesToInstall.length) {
     depArgs = [
       ...args,
       ...dependenciesToInstall.map(([dependency, version]) => {
-        return `${dependency}@${version}`;
+        return `${dependency}@${version}`
       }),
-    ];
+    ]
   }
-  const devDependenciesToInstall = Object.entries(
-    templatePackage.devDependencies || {}
-  );
+  const devDependenciesToInstall = Object.entries(templatePackage.devDependencies || {})
   if (devDependenciesToInstall.length) {
     devDepArgs = [
       ...args,
       ...devDependenciesToInstall.map(([dependency, version]) => {
-        return `${dependency}@${version}`;
+        return `${dependency}@${version}`
       }),
-    ];
+    ]
   }
 
   if (templateName && depArgs.length > 1) {
-    console.log();
-    console.log(`Installing template dependencies using ${command}...`);
+    console.log()
+    console.log(`Installing template dependencies using ${command}...`)
 
-    const proc = spawn.sync(command, depArgs, { stdio: "inherit" });
+    const proc = spawn.sync(command, depArgs, { stdio: 'inherit' })
     if (proc.status !== 0) {
-      console.error(`\`${command} ${depArgs.join(" ")}\` failed`);
-      return;
+      console.error(`\`${command} ${depArgs.join(' ')}\` failed`)
+      return
     }
   }
   if (templateName && devDepArgs.length > 1) {
-    console.log();
-    console.log(`Installing template devDependencies using ${command}...`);
+    console.log()
+    console.log(`Installing template devDependencies using ${command}...`)
 
-    const proc = spawn.sync(
-      command,
-      [...devDepArgs, useYarn ? "--dev" : "--save-dev"],
-      {
-        stdio: "inherit",
-      }
-    );
+    const proc = spawn.sync(command, [...devDepArgs, useYarn ? '--dev' : '--save-dev'], {
+      stdio: 'inherit',
+    })
     if (proc.status !== 0) {
-      console.error(`\`${command} ${devDepArgs.join(" ")}\` failed`);
-      return;
+      console.error(`\`${command} ${devDepArgs.join(' ')}\` failed`)
+      return
     }
   }
-  if (devDepArgs.find((arg) => arg.includes("typescript"))) {
-    console.log();
-    verifyTypeScriptSetup();
+  if (devDepArgs.find((arg) => arg.includes('typescript'))) {
+    console.log()
+    verifyTypeScriptSetup()
   }
 
   // Remove template
-  console.log(`Removing template package using ${command}...`);
-  console.log();
+  console.log(`Removing template package using ${command}...`)
+  console.log()
 
   const proc = spawn.sync(command, [remove, templateName], {
-    stdio: "inherit",
-  });
+    stdio: 'inherit',
+  })
   if (proc.status !== 0) {
-    console.error(`\`${command} ${args.join(" ")}\` failed`);
-    return;
+    console.error(`\`${command} ${args.join(' ')}\` failed`)
+    return
   }
 
   // Create git commit if git repo was initialized
   if (initializedGit && tryGitCommit(appPath)) {
-    console.log();
-    console.log("Created git commit.");
+    console.log()
+    console.log('Created git commit.')
   }
 
   // Display the most elegant way to cd.
   // This needs to handle an undefined originalDirectory for
   // backward compatibility with old global-cli's.
-  let cdpath;
+  let cdpath
   if (originalDirectory && path.join(originalDirectory, appName) === appPath) {
-    cdpath = appName;
+    cdpath = appName
   } else {
-    cdpath = appPath;
+    cdpath = appPath
   }
 
   // Change displayed command to yarn instead of yarnpkg
-  const displayedCommand = useYarn ? "yarn" : "npm";
+  const displayedCommand = useYarn ? 'yarn' : 'npm'
 
-  console.log();
-  console.log(`Success! Created ${appName} at ${appPath}`);
-  console.log("Inside that directory, you can run several commands:");
-  console.log();
-  console.log(chalk.cyan(`  ${displayedCommand} start`));
-  console.log("    Starts the development server.");
-  console.log();
-  console.log(
-    chalk.cyan(`  ${displayedCommand} ${useYarn ? "" : "run "}build`)
-  );
-  console.log("    Bundles the app into static files for production.");
-  console.log();
-  console.log("We suggest that you begin by typing:");
-  console.log();
-  console.log(chalk.cyan("  cd"), cdpath);
-  console.log(`  ${chalk.cyan(`${displayedCommand} start`)}`);
-  console.log();
-  console.log("Happy hacking!");
-};
+  console.log()
+  console.log(`Success! Created ${appName} at ${appPath}`)
+  console.log('Inside that directory, you can run several commands:')
+  console.log()
+  console.log(chalk.cyan(`  ${displayedCommand} start`))
+  console.log('    Starts the development server.')
+  console.log()
+  console.log(chalk.cyan(`  ${displayedCommand} ${useYarn ? '' : 'run '}build`))
+  console.log('    Bundles the app into static files for production.')
+  console.log()
+  console.log('We suggest that you begin by typing:')
+  console.log()
+  console.log(chalk.cyan('  cd'), cdpath)
+  console.log(`  ${chalk.cyan(`${displayedCommand} start`)}`)
+  console.log()
+  console.log('Happy hacking!')
+}
